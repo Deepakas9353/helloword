ac8c3e13c7bce1a5f449a04d9e8f81dc97357f7a
deepak A S <deepaks.9141@gmail.com>
2024-10-30 10:58:59 +0530

    locally changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ public class Main {
         System.out.println("Hello World");
     }
     public static void sum() {
-        int a=1+2;
-        System.out.println(a);
         System.out.println("method to sum");
     }
 }
